376867d0b84ddc731724d3cabccc38c143db7265
phuocanPC <phuocantd@gmail.com>
2020-01-14 22:38:28 +0700

    Updates

---
+++ precache-manifest.7a8761b4e4ae15d633c6acb3fab53d04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20fa19b3ec03765b079859639e38603e",
+    "url": "/my-cv/index.html"
+  },
+  {
+    "revision": "e0e04f98fefd4c5485ed",
+    "url": "/my-cv/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "20f064a5b8aa04b0b674",
+    "url": "/my-cv/static/js/2.8b6fc889.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-cv/static/js/2.8b6fc889.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e0e04f98fefd4c5485ed",
+    "url": "/my-cv/static/js/main.62aa45ca.chunk.js"
+  },
+  {
+    "revision": "b0142c1b9ea017e4bb38",
+    "url": "/my-cv/static/js/runtime-main.74b30775.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-cv/static/media/logo.5d5d9eef.svg"
+  }
+]);
